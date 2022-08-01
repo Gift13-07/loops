1526b4e4f4f18b68938b391f958f76f73f2877a8
Gift13-07 <giftchinwendu07@gmail.com>
2022-08-01 21:12:36 +0600

    git commit

--- filter.js
+++ filter.js
@@ -1,7 +1,6 @@
-//The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.//
-let words = ['spray', 'limit', 'elite', 'best', 'lovely', 'cool'];
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-let result = words.filter(word => word.length > 6);
+const result = words.filter(word => word.length > 6);
 
 console.log(result);
-// expected output: Array ["best", "lovely", "cool"]
+// expected output: Array ["exuberant", "destruction", "present"]
